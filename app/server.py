2ff5d5490586f3a748dad3e94c4e09687ee40863
chloepouprom <Chloe@wpa074030.wireless.mcgill.ca>
2015-02-21 17:09:50 -0500

    send/receive emails

--- app/server.py
+++ app/server.py
@@ -2,19 +2,54 @@ __author__ = 'mili'
 import json
 import os
 import flask
+import smtplib, smtpd
 from flask import Flask, session, request, render_template, flash, redirect, url_for, g, jsonify
 
 app = Flask(__name__)
 
+
+USERNAME = 'c8469ff849bfa3c7d7a749998c85b94c'
+PASSWORD = 'f0546ae2b1486c2838cd3a7f36863246'
+SENDER = 'chloe@parse-in1.mailjet.com'
+
 from threading import Thread
 
-@app.route("/email_processor", methods = ['POST'])
+@app.route("/email_process", methods = ['POST'])
 def email_processor():
    data = request.data
    dataDict = json.loads(data)
    body = dataDict['Text-part']
+   sender = dataDict['Sender']
+   subject = dataDict['Subject']
+
+   print(sender)
+   print subject
    print(body)
-   #send email back out
+
+   # Send e-mail back to sender
+   send_mail(body, sender, subject)
+
+
+   return body
+
+
+def send_mail(text, receiver, subject):
+   message = "\r\n".join([
+        "From: %s" % SENDER,
+        "To: %s" % receiver,
+        "Subject: %s" % subject,
+       text
+        ])
+   try:
+      server = smtplib.SMTP('in-v3.mailjet.com:587')
+      server.starttls()
+      server.login(USERNAME,PASSWORD)
+      server.sendmail(SENDER, receiver, message)
+      server.quit()
+      print "Successfully sent email to %s" % receiver
+   except smtplib.SMTPException:
+      print "Error: unable to send email to %s" % receiver
+
 
 if __name__ =="__main__":
     app.run(debug =True)
